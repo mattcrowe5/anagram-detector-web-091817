3b0927c37db3367d7cb57f4a8b6679c03c80364f
mattcrowe5 <crowe.matthew5@gmail.com>
2017-09-26 17:33:46 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    #work on this later
+    array.select do |backword|
+      backword.chars.sort == @word.chars.sort
+    end
+  end
+
+end
